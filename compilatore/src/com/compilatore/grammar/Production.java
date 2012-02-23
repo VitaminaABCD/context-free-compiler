ce692152c7c6233a73107a1891433a7d07bd258c
sottilepierluigi <sottilepierluigi@28e20d46-7ed8-d506-fc78-cbff12b26270>
2012-02-23 17:04:01 +0000

    creazione  di P in Item e modifiche al costruttore in Prodaction.....

--- compilatore/src/com/compilatore/grammar/Production.java
+++ compilatore/src/com/compilatore/grammar/Production.java
@@ -5,63 +5,63 @@ import java.util.List;
 
 public class Production {
 
-	private String left;
-	private String right;
-	private List <String> rightList;
+        private String left;
+        private String right;
+        private List <String> rightList;
 
 
-	public Production(){
-		left="";
-		right="";
-	}
-	
-	public Production(String lt, String rt){
-		left=lt;
-		right=rt;
-		rightList = new ArrayList<String>();
-		char[] temp = right.toCharArray();
-		for(int i=0;i<temp.length;i++)
-			this.rightList.add(Character.toString(temp[i]));
-	}
-	
-	public Production(String lt, String rt,boolean flag){
-		left=lt;
-		right=rt;
-		rightList = new ArrayList<String>();
-	}
-	
-	public String getLeft() {
-		return left;
-	}
+        public Production(){
+                left="";
+                right="";
+        }
+        
+        public Production(String lt, String rt){
+                left=lt;
+                right=rt;
+                rightList = new ArrayList<String>();
+                char[] temp = right.toCharArray();
+                for(int i=0;i<temp.length;i++)
+                        this.rightList.add(Character.toString(temp[i]));
+        }
+        
+        public Production(String lt, String rt,boolean flag){
+                left=lt;
+                right=rt;
+                rightList = new ArrayList<String>();
+        }
+        
+        public String getLeft() {
+                return left;
+        }
 
-	public void setLeft(String left) {
-		this.left = left;
-	}
+        public void setLeft(String left) {
+                this.left = left;
+        }
 
-	public String getRight() {
-		return right;
-	}
+        public String getRight() {
+                return right;
+        }
 
-	public void setRight(String right) {
-		this.right = right;
-	}
-	
-	public List<String> getRightList() {
-		return rightList;
-	}
+        public void setRight(String right) {
+                this.right = right;
+        }
+        
+        public List<String> getRightList() {
+                return rightList;
+        }
 
-	public void setRightList(List<String> rightList) {
-		this.rightList = rightList;
-	}
-	/**
-	 * restiuisce una stringa formattata nella forma ass::=espr
-	 */
-	@Override
-	public String toString() {
-		if (right.equals(" "))
-			return left + "::=" + "eps";
-		else{
-				return left + "::=" + right ;
-		}
-	}
+        public void setRightList(List<String> rightList) {
+                this.rightList = rightList;
+        }
+        /**
+         * restiuisce una stringa formattata nella forma ass::=espr
+         */
+        @Override
+        public String toString() {
+                if (right.equals(" "))
+                        return left + "::=" + "eps";
+                else{
+                                return left + "::=" + right ;
+                }
+        }
 }

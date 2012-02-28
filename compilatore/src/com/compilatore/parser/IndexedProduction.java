4ca8363dacd16e7ed48cb4e068aa64784794a0df
sottilepierluigi <sottilepierluigi@28e20d46-7ed8-d506-fc78-cbff12b26270>
2012-02-28 16:42:17 +0000

    daghfd

--- compilatore/src/com/compilatore/parser/IndexedProduction.java
+++ compilatore/src/com/compilatore/parser/IndexedProduction.java
@@ -1,8 +1,6 @@
 package com.compilatore.parser;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.compilatore.grammar.Production;

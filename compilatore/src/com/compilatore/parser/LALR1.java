ec1224f7f880f20f9823a06b3d01dcc7f89d8061
paulux84@gmail.com <paulux84@gmail.com@28e20d46-7ed8-d506-fc78-cbff12b26270>
2012-02-29 22:47:20 +0000

    Modificati i for in generateLookahead con gli iterator credendo fosse la cuasa della mancata scrittura dei risultati produzioni kernel dell'automa LALR1 --> IL PROBLEMA NON E' STATO RISOLTO

--- compilatore/src/com/compilatore/parser/LALR1.java
+++ compilatore/src/com/compilatore/parser/LALR1.java
@@ -85,11 +85,15 @@ public class LALR1 extends LR0{
 	}
 	
 	private void generateLookahead(IndexedProduction p) {
-		for(State s : this.automa.getStates()){
-			for(IndexedProduction k : s.getKernels()){
-				if(p.compare(k)) {
-					k.getLookahead().addAll(p.getLookahead());
-					logger.debug("aggiunto " + p.getLookahead() + " a " + k.toString());
+		Iterator<State> s = this.automa.getStates().iterator();
+		while(s.hasNext()){
+			State st = s.next();
+			Iterator<IndexedProduction> k = st.getKernels().iterator();
+			while(k.hasNext()){
+				IndexedProduction kn = k.next();
+				if(p.compare(kn)) {
+					kn.getLookahead().addAll(p.getLookahead());
+					logger.debug("aggiunto " + p.getLookahead() + " a " + kn.toString());
 				}
 			}
 		}

6d3606ca591af513894289fc5be9fb4c669b3664
paulux84@gmail.com <paulux84@gmail.com@28e20d46-7ed8-d506-fc78-cbff12b26270>
2012-03-15 02:39:54 +0000

    Inserita la lista di simboli...in lavorazione per l'albero...

--- compilatore/src/com/Parse/Parser.java
+++ compilatore/src/com/Parse/Parser.java
@@ -1,6 +1,7 @@
 package com.Parse;
 
 
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,9 +17,11 @@ public class Parser {
 	static Logger logger = Logger.getLogger(Parser.class.getName());
 	
 	private Stack<String> stack;
+	private String simbol;
 	private String input;
 	private IGrammar grammar;
 	private List<HistoryElement> history;
+	
 	
 	private Hashtable<String,List<String>> actionTable;
 	private Hashtable<String,List<String>> gotoTable;
@@ -28,6 +31,7 @@ public class Parser {
 		this.stack.add("0");
 		this.input="";
 		this.grammar=null;
+		this.simbol = "";
 		this.history = new LinkedList<HistoryElement>();
 		this.actionTable = new Hashtable<String, List<String>>();
 		this.gotoTable = new Hashtable<String, List<String>>();
@@ -41,6 +45,7 @@ public class Parser {
 		this.history = new LinkedList<HistoryElement>();
 		this.actionTable = new Hashtable<String, List<String>>();
 		this.gotoTable = new Hashtable<String, List<String>>();
+		this.simbol = "";
 	}
 	
 	
@@ -55,6 +60,7 @@ public class Parser {
 		this.actionTable=action;
 		this.gotoTable=goTo;
 		this.history = new LinkedList<HistoryElement>();
+		this.simbol = "";
 	}
 
 	public void setActionTable(Hashtable<String, List<String>> actionTable) {
@@ -110,28 +116,51 @@ public class Parser {
 			}
 			String act = actionTable.get(temp).get(state);   //valore della tabella action riga(lo stato) e colonna (il carattere)
 			char [] splitAct = act.toCharArray();
-			if(splitAct[0]=='s'){ //se inizia per 's' è uno schift
+			if(splitAct[0]=='s'){ //se inizia per 's' e' uno schift
 				this.stack.push(String.valueOf(splitAct[1]));
-				String[] tp = new String[stack.size()]; 
-				stack.copyInto(tp);
-				this.history.add(new HistoryElement(tp));
-				logger.debug("inserito simbolo nello stack e nella storia a seguito di uno shift");
-				index++;
+				this.simbol+=temp;								//aggiunge il carattere nello stack simboli
+/*   salva   */	String[] tp = new String[stack.size()]; 
+/*cronologia */	stack.copyInto(tp);
+				String tempSimbol = this.simbol.toString();
+				this.history.add(new HistoryElement(tp,null,tempSimbol));
+				logger.debug("inserito simbolo nello stack e nella storia a seguito di uno shift");				
+//////END*/////			
+				index++;	//punta "l'indice" al prossimo carattere
 			}else if(act.equals("acc")) break;  //accetta
 			else if(act.equals("err")) return RESULT.ERROR;
 			else{ //e' una riduzione
-				String [] production=act.split("::=");
+				String [] production=act.split("::=");		//act=produzione nella actionTable
 				for(int i=0;i<production[1].length();i++){ //rimuove beta simboli dallo stack
 					stack.pop();
 				}
-				state=Integer.parseInt(stack.lastElement());
-				this.stack.push(gotoTable.get(production[0]).get(state));
-				String [] tp = new String[stack.size()]; 
+				state=Integer.parseInt(stack.lastElement());	//prende lo stato sulla testa dello stack
+				this.stack.push(gotoTable.get(production[0]).get(state));	//inserisce nello stack il valore della gotoTable corristondente a [stato][non_terminale_della_produzione_nella_actionTable]
+				this.simbol=replaceLast(this.simbol,production[1], production[0]); // esegue la riduzione nello stack simboli
+				String tempSimbol =simbol.toString();
+				String [] tp = new String[stack.size()]; 					//formatta lo stack in uno String[] per passarlo all'history
 				stack.copyInto(tp);
-				this.history.add(new HistoryElement(tp, new Production(production[0], production[1])));
+				this.history.add(new HistoryElement(tp, new Production(production[0], production[1]),tempSimbol)); //aggiunge l'elemento nella storia
 				logger.debug("inserito simbolo e produzione nello stack e nella storia a seguito di un reduce");
 			}
 		}
 		return RESULT.ACCEPT;
 	}
+	
+	
+	/*	(?s)    # enable dot-all option
+		A       # match the character 'A'
+		B       # match the character 'B'
+		(?!     # start negative look ahead
+  		.*?    	#   match any character and repeat it zero or more times, reluctantly
+  		A      	#	match the character 'A'
+  		B     	# 	match the character 'B'
+		)       # end negative look a
+	 * 
+	 */
+	private static String replaceLast(String text, String regex, String replacement) {
+        return text.replaceFirst("(?s)"+regex+"(?!.*?"+regex+")", replacement);
+    }
+	
+	
+	
 }

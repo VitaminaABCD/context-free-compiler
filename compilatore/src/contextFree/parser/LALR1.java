92c9b35f4d3550fc991218b1bf2ec1e557fa6904
sottilepierluigi <sottilepierluigi@28e20d46-7ed8-d506-fc78-cbff12b26270>
2012-03-18 11:19:53 +0000

    sostituito à con a'

--- compilatore/src/contextFree/parser/LALR1.java
+++ compilatore/src/contextFree/parser/LALR1.java
@@ -360,9 +360,9 @@ public class LALR1 extends LR0{
 		else
 			//se no evidenzio lo stato di ambiguit�
 			if(j>=0)
-				ambiguo+="\nAmbiguit� allo stato "+ i+": "+ actionTable[i][x]+ ", shift " + j ;
+				ambiguo+="\nAmbiguita' allo stato "+ i+": "+ actionTable[i][x]+ ", shift " + j ;
 			else
-				ambiguo+="\nAmbiguit� allo stato "+ i+ ": "+ actionTable[i][x]+ ", "+action;
+				ambiguo+="\nAmbiguita' allo stato "+ i+ ": "+ actionTable[i][x]+ ", "+action;
 		return esito;
 	}
 	@Override

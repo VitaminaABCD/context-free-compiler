d1ab5a12f862509c6c1ea0f35ff35045c15faf85
paulux84 <paulux84@gmail.com>
2012-04-04 19:43:01 +0000

    rifattorizzato lo split dei caratteri di input del parserProgram con il metodo in SplitInSymbols piccole modifiche a SplitInSymbols

--- compilatore/src/inputParser/SplitInSymbols.java
+++ compilatore/src/inputParser/SplitInSymbols.java
@@ -15,10 +15,11 @@ public class SplitInSymbols implements Runnable {
 	static Logger logger = Logger.getLogger(SplitInSymbols.class.getName());
 	
 	private String obj;
-	private List<String> result,Symbols;
+	private List<String> result,symbols;
 	//Constructor
-	public SplitInSymbols(){this.obj=null;this.result=this.Symbols=null;}
-	public SplitInSymbols(String in,List<String> symb){this.obj=in;this.Symbols=symb;}
+	public SplitInSymbols(){this.obj=null;this.result=this.symbols=null;}
+	public SplitInSymbols(String in){this.obj=in;}	
+	public SplitInSymbols(String in,List<String> symb){this.obj=in;this.symbols=symb;}
 	/**
 	 * Constructor with input String
 	 * @param ob the input string
@@ -28,7 +29,7 @@ public class SplitInSymbols implements Runnable {
 	public SplitInSymbols(String ob,List<String> res,List<String> symb){
 		this.obj=ob;
 		this.result=res;
-		this.Symbols = symb;
+		this.symbols = symb;
 		}
 	//
 	
@@ -45,10 +46,15 @@ public class SplitInSymbols implements Runnable {
 		this.result = result;
 	}
 	public List<String> getSymbols() {
-		return Symbols;
+		return symbols;
 	}
-	public void setSymbols(List<String> symbols) {
-		Symbols = symbols;
+	public void setSymbols(List<String> symb) {
+		this.symbols = symb;
+	}
+	
+	public void addSymbols(List<String> symb) {
+		if(this.symbols==null) this.symbols = new LinkedList<String>();
+		this.symbols.addAll(symb);
 	}
 	
 	public void run() {
@@ -56,37 +62,42 @@ public class SplitInSymbols implements Runnable {
 		
 		int size=obj.length();
 		int index=0;
-		boolean founded=false;
+		boolean founded;
 		
 		while(index<size){
+			founded=false;
 			String temp=Character.toString(obj.charAt(index));
 			int max=0,min=0;
 			Pattern pattern = Pattern.compile(Pattern.quote(temp)+".*");
-			for(String k : this.Symbols){
+			for(String k : this.symbols){
 				Matcher matcher = pattern.matcher(k);
 				while(matcher.find()){
 					int sz=matcher.group().length();
 					if(sz>max) max= sz;
 					if(sz<min) min= sz;
 				}
-			}				
+			}
+			
 			while(max>min){		//entra solo se ne ha trovato almeno uno 
 				if(index+max<obj.length()) temp=obj.substring(index,index+max);		//controlla la più lunga sequenza di caratteri
 	 			else temp=obj.substring(index);
-				if(Symbols.contains(temp)){
-					founded=true;
+				if(symbols.contains(temp)){
 					this.result.add(temp);
 					index+=max;
+					founded=true;
 					break;
 				}
 				max--;
 			}
+			
+			if(!founded) {
+				logger.info("La grammatica corrente non contiene '" + temp + "' tra i caratteri ammessi");
+				this.result=null;
+				return;
+			}
 		}
 		
-		if(!founded) {
-			logger.warning("La stringa di ingresso contiene caratteri non consentiti");
-			this.result=null;
-		}
+
 	}
 	
 	public int count(){
